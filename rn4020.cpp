0c07ddeaebaa6b4bdb6e9dcade6dba3b2f2f91ac
onetwosteph <stephanie.amati@gmail.com>
2016-10-24 18:28:48 +0100

    Started to write the commands.

---
+++ rn4020.cpp
@@ -0,0 +1,5 @@
+int get_baudrate(int baud_nb){
+	switch(baud_nb){
+		case 0: return 
+	}
+}
--- rn4020.h
+++ rn4020.h
@@ -1,6 +1,18 @@
 #ifndef rn4020_h
 #define rn4020_h
 
-// the #include statment and code go here...
+// Simple get commands
+#define GET_BAUD_NB			"GB"
+#define
+
+// Other
+const int baud_rates[8] = {2400, 9600, 19200, 38400, 115200, 230400, 460800, 921600};
+
+// Feedbacks
+#define ERROR				"ERR"
+#define OK					"AOK"
+
+// Get function
+
 
 #endif
